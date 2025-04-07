c4a0098ad8344919d453aea42d3da7db4304fde9
JarkkoKarki <karkijarkko@gmail.com>
2025-04-07 19:56:30 +0300

    Add SingleView component and enhance MediaRow for item selection; update Home component to manage selected media state

--- src/components/MediaRow.jsx
+++ src/components/MediaRow.jsx
@@ -1,7 +1,12 @@
 import PropTypes from 'prop-types';
 
 const MediaRow = (props) => {
-  const {item} = props;
+  const {item, setSelectedItem} = props;
+
+  const handleClick = () => {
+    setSelectedItem(item);
+  };
+
   return (
     <>
       <tr key={item.media_id}>
@@ -13,6 +18,9 @@ const MediaRow = (props) => {
         <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
         <td>{item.filesize}</td>
         <td>{item.media_type}</td>
+        <td>
+          <button onClick={handleClick}>View</button>
+        </td>
       </tr>
     </>
   );
@@ -20,6 +28,7 @@ const MediaRow = (props) => {
 
 MediaRow.propTypes = {
   item: PropTypes.object.isRequired,
+  setMediaItem: PropTypes.func.isRequired,
 };
 
 export default MediaRow;
